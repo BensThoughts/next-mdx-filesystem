85c944d2ce18d90683ae4834abf6a6efe9390fa9
Benjamin Blumenfeld-Jones <bensthoughts@gmail.com>
2021-11-21 07:57:43 -0700

    use const instead of function

--- src/file/index.ts
+++ src/file/index.ts
@@ -1,9 +1,8 @@
 import fs from 'fs';
 
-
-export function readDir(dir: string) {
+export const readDir = (dir: string): fs.Dirent[] => {
   return fs.readdirSync(dir, {withFileTypes: true});
-}
+};
 
 export const readFile = (path: string): string => {
   return fs.readFileSync(path, 'utf-8');
@@ -13,15 +12,15 @@ export const doesPathExist = (path: string): boolean => {
   return fs.existsSync(path);
 };
 
-export const isPathDir = (path: string) => {
+export const isPathDir = (path: string): boolean => {
   return fs.statSync(path).isDirectory();
 };
 
-export const isPathFile = (path:string) => {
+export const isPathFile = (path:string): boolean => {
   return fs.statSync(path).isFile();
 };
 
-export const getFileModifiedDate = (path: string) => {
+export const getFileModifiedDate = (path: string): string => {
   const fullDate = fs.statSync(path).mtime;
   const date = `${fullDate.getUTCFullYear()}-${fullDate.getUTCMonth() + 1}` +
     `-${fullDate.getUTCDate()}`;
--- src/path/index.ts
+++ src/path/index.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import fs from 'fs';
+import {Dirent} from 'fs';
 
 import {
   POSTS_DIR,
@@ -7,40 +7,36 @@ import {
   EXCLUDED_PROD_DIRS,
 } from '../config/index.js';
 
-// export const getPath = (...pathSegment: string[]): string => {
-//   return path.resolve(process.cwd(), ...pathSegment);
-// };
-
 export const getFullPathFromSlug = (slug: string): string => {
   slug = slug.charAt(0) === path.sep ? slug.substr(1) : slug;
   return path.resolve(POSTS_DIR, slug);
 };
 
-export function slugToArray(slug: string): string[] {
+export const slugToArray = (slug: string): string[] => {
   const slugArr = slug.replace(/^\//, '').split(path.sep);
   return slugArr;
-}
+};
 
-export function slugArrayToString(slug: string[]): string {
+export const slugArrayToString = (slug: string[]): string => {
   return path.join(...slug);
-}
+};
 
-export function getFileName(fullPath: string): string {
+export const getFileName = (fullPath: string): string => {
   return path.basename(fullPath);
-}
+};
 
-export function getDirIndex(dirPath: string): string {
+export const getDirIndex = (dirPath: string): string => {
   return path.join(dirPath, DIR_INDEX_FILE);
-}
+};
 
-export function getSlugFromFullPath(fullPath: string): string {
+export const getSlugFromFullPath = (fullPath: string): string => {
   return fullPath
       .replace(POSTS_DIR, '')
       .replace(path.sep, '/')
       .replace('.mdx', '');
-}
+};
 
-export function getDirentData(cwd: string, dirent: fs.Dirent) {
+export const getDirentData = (cwd: string, dirent: Dirent) => {
   const fullPath = path.join(cwd, dirent.name);
   const isMdx = path.extname(fullPath) === '.mdx';
   const isDirectory = dirent.isDirectory();
@@ -53,4 +49,4 @@ export function getDirentData(cwd: string, dirent: fs.Dirent) {
     isDirectory,
     isExcludedPath,
   };
-}
+};
