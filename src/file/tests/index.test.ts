14258bd5788afc5180be0a1702d42fae160701d4
Benjamin Blumenfeld-Jones <bensthoughts@gmail.com>
2021-11-27 09:29:33 -0700

    fixed test message

--- src/file/tests/index.test.ts
+++ src/file/tests/index.test.ts
@@ -1,11 +1,9 @@
 import {formatDate} from '..';
 
-describe('getFileModifiedDate', () => {
+describe('formatDate', () => {
   test('returns the correct file modified date format', () => {
     const date = new Date(2021, 10, 22);
     expect(formatDate(date))
         .toStrictEqual('2021-11-22');
   });
 });
-
-
