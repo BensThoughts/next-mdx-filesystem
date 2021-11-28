41d6a0b5407309b760cbb811f84470d274c52507
Benjamin Blumenfeld-Jones <bensthoughts@gmail.com>
2021-11-28 08:16:02 -0700

    test when config path env is undefined

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,52 +10,49 @@ import {
  * errors.
  */
 
-const getPath = (...pathSegment: string[]): string => {
+export const getPath = (...pathSegment: string[]): string => {
   return path.resolve(process.cwd(), ...pathSegment);
 };
 
 const defaultConfig: GlobalConfig = {
-  'postsDir': getPath('mdx-posts'),
+  'postsDir': 'mdx-posts',
   'excludedProdDirs': [],
   'dirIndexFile': 'index.yaml',
 };
 
-export function loadConfigFile<T>(
+export function loadConfigFile(
     path: string,
-): T | undefined {
+): Partial<GlobalConfig> {
   if (fs.existsSync(path)) {
     const config = fs.readFileSync(path, 'utf-8');
-    const userConfig = JSON.parse(config) as T;
+    const userConfig = JSON.parse(config);
     return userConfig;
   }
-  return undefined;
+  return {};
 };
 
 export function loadConfig(configPath: string): GlobalConfig {
-  const baseUserConfig =
-    loadConfigFile<Partial<GlobalConfig>>(configPath);
-  if (baseUserConfig) {
-    const fullPathPostsDir = baseUserConfig.postsDir ?
+  const baseUserConfig = loadConfigFile(configPath);
+  const fullPathPostsDir = baseUserConfig.postsDir ?
       getPath(baseUserConfig.postsDir) :
-      defaultConfig.postsDir;
-    const userConfig: Partial<GlobalConfig> = {
-      ...baseUserConfig,
-      postsDir: fullPathPostsDir,
-    };
-    return {
-      ...defaultConfig,
-      ...userConfig,
-    };
-  } else {
-    return defaultConfig;
-  }
+      getPath(defaultConfig.postsDir);
+  const userConfig = {
+    ...baseUserConfig,
+    postsDir: fullPathPostsDir,
+  };
+  return {
+    ...defaultConfig,
+    ...userConfig,
+  };
 };
 
-export const configPath = process.env.MDX_FILESYSTEM_CONFIG_PATH ?
+export function getConfigPath(): string {
+  return process.env.MDX_FILESYSTEM_CONFIG_PATH ?
   getPath(process.env.MDX_FILESYSTEM_CONFIG_PATH) :
   getPath('mdx-filesystem.config.json');
+}
 
-const config = loadConfig(configPath);
+const config = loadConfig(getConfigPath());
 
 const productionConfig = {
   POSTS_DIR: config.postsDir,
--- src/config/tests/index.test.ts
+++ src/config/tests/index.test.ts
@@ -1,22 +1,32 @@
 /*eslint spaced-comment: ["off"] */
 /*eslint max-len: ["off"] */
 import {
+  // loadConfigFile,
   loadConfig,
-  configPath,
+  getConfigPath,
 } from '..';
+
+import path from 'path';
+
+describe('getConfigPath', () => {
+  test.concurrent('should return default when ENV is not set', () => {
+    const tempEnv = process.env.MDX_FILESYSTEM_CONFIG_PATH;
+    delete process.env.MDX_FILESYSTEM_CONFIG_PATH;
+    expect(path.basename(getConfigPath())).toBe('mdx-filesystem.config.json');
+    process.env.MDX_FILESYSTEM_CONFIG_PATH = tempEnv;
+  });
+});
+
 
 describe('loadConfig', () => {
   test.concurrent('should return a user config when config file exists', () => {
-    const config = loadConfig(configPath);
+    const config = loadConfig(getConfigPath());
     expect(config.dirIndexFile).toBe('index.yaml');
     expect(config.excludedProdDirs).toStrictEqual(['drafts']);
   });
   test.concurrent('should return a default config when config path does not exist', () => {
-    const tempEnv = process.env.MDX_FILESYSTEM_CONFIG_PATH;
-    process.env.MDX_FILESYSTEM_CONFIG_PATH = 'bad.path';
     const config = loadConfig('badPath');
     expect(config.dirIndexFile).toBe('index.yaml');
     expect(config.excludedProdDirs).toStrictEqual([]);
-    process.env.MDX_FILESYSTEM_CONFIG_PATH = tempEnv;
   });
 });
